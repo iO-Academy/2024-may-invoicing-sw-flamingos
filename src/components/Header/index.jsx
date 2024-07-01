7a9a2911268a07c4ee369370d219b573a332faf1
Ivan_Savchuk <vanya.savchuk@gmail.com>
2024-07-01 15:57:12 +0100

    story1task1next  steps

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+
+export default function Header() {
+    const [unpaid, setUnpaid] = useState(0)
+    const [data, setData] = useState([])
+    useEffect(
+        fetch('https://invoicing-api.dev.io-academy.uk/invoices')
+        .then(res => res.json())
+        .then((invoices) => {
+            setData(invoices.data)  
+}, [])
+    )
+
+    return (
+        function UnpaidCounter() {
+            data.map(invoice.status === "2" && setUnpaid(unpaid + 1)
+
+            )
+        }
+        <header>
+           <h1>Invoices</h1> 
+           <p>There are {unpaid} unpaid invoices</p>
+        </header>
+    )
+}
