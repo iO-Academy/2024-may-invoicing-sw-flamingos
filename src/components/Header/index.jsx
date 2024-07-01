abd0a51b3fc0bcf3de5a8f7d9e5a0dee3ac94fae
Andy Bool <andybool50@gmail.com>
2024-07-01 16:39:58 +0100

    With Ash's help fixed the unpaid invoice counter function

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,25 +3,34 @@ import { useEffect, useState } from "react"
 export default function Header() {
     const [unpaid, setUnpaid] = useState(0)
     const [data, setData] = useState([])
-    useEffect(
-        fetch('https://invoicing-api.dev.io-academy.uk/invoices')
-        .then(res => res.json())
-        .then((invoices) => {
-            setData(invoices.data)  
-}, [])
-    )
 
-    function UnpaidCounter() {
-        data.map(invoice.status === "2" && setUnpaid(unpaid + 1)
-        )
-        console.log(unpaid)
-    }
+    useEffect(() => {
+        fetch('https://invoicing-api.dev.io-academy.uk/invoices')
+            .then(res => res.json())
+            .then((invoices) => {
+                setData(invoices.data)
+
+                let counter = 0
+
+                invoices.data.forEach((invoice) => {
+                    invoice.status === "2" && counter ++
+                })
+                setUnpaid(counter)
+            })}, [])
+
+
+    /* function unpaidCounter() {
+         data.map((invoice) => {
+                invoice.status === "2" && setUnpaid(unpaid + 1)
+         })
+         console.log(unpaid)
+     } */
 
     return (
-        
+
         <header>
-           <h1>Invoices</h1> 
-           <p>There are {unpaid} unpaid invoices</p>
-    </header>
+            <h1>Invoices</h1>
+            <p>There are {unpaid} unpaid invoices</p>
+        </header>
     )
 }
