18790832488ea65a533ad0c52156bf7d3356ea52
Andy Bool <andybool50@gmail.com>
2024-07-02 11:53:13 +0100

    created status container div

--- src/components/Statusbox/index.jsx
+++ src/components/Statusbox/index.jsx
@@ -9,15 +9,19 @@ export default Statusbox(invoiceStatus, invoiceDue) {
                 let statusContainer = ''
 
             function status(() => {
-                if ({invoiceStatus} === 'Paid') {
-statusContainer = <div className="border border-green-400">
-    <h3 className="p-3 text-green-400">{invoiceStatus}</h3>
-</div>
+                if (invoiceStatus === 'Paid') {
+            statusContainer = '<div className="border border-green-400">
+                                <h3 className="p-3 text-green-400">invoiceStatus</h3>
+                                </div>'
                 }
 
             )}
 
     return (
+
+        <div>
+        statusContainer
+        </div>
 
     )
 }
