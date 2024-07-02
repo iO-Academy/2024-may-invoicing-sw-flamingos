edf45010368916a83d85f52ab9dafc8bdd7c4bf0
Ivan_Savchuk <vanya.savchuk@gmail.com>
2024-07-02 10:07:02 +0100

    Footer was created

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 export default function App() {
@@ -5,6 +6,7 @@ export default function App() {
   return (
     <>
         <Header />
+        <Footer/>
     </>
   )
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+
+    return (
+
+        <footer className="bg-slate-50 p-4">
+            <p className="pb-3" >&copy; Copyright 2024</p>
+        </footer>
+    )
+}
