0b7fada70e560925396d30d6781631211047ef49
Ivan_Savchuk <vanya.savchuk@gmail.com>
2024-07-02 15:56:43 +0100

    bg was added

--- src/components/InvoiceList/index.jsx
+++ src/components/InvoiceList/index.jsx
@@ -15,7 +15,7 @@ export default function InvoiceList() {
     }, [])
 
     return (
-        <div className="flex flex-col items-center px-16">
+        <div className="flex flex-col items-center px-16 bg-slate-50">
             {invoices.map(invoice => {
                             return (
                             <InvoiceSummary key={invoice.id} invoiceID={invoice.invoice_id} invoiceTotal={invoice.invoice_total} invoiceName={invoice.name} invoiceStat={invoice.status_name} invoiceDue={invoice.due} />
