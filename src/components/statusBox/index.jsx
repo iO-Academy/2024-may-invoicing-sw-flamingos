2710dafcc0169d08de0e9c810197018b5062c2d3
Max Estrella <maxestrella565@gmail.com>
2024-07-02 15:47:25 +0100

    Attempting to fix capital S problem

--- src/components/statusBox/index.jsx
+++ src/components/statusBox/index.jsx
@@ -1,32 +1,5 @@
 export default function StatusBox({invoiceStatus, invoiceDue}) {
 
-             /*   let statusContainer = ''
-
-
-            function status() {
-                if (invoiceStatus === 'Paid') {
-            return statusContainer = 
-                                `<ul class="border border-green-400">
-                                <li class="p-3 text-green-400">${billStatus}</li>
-                                </ul>`
-                }
-
-                    else if (invoiceStatus === 'Pending') {
-                       return statusContainer = 
-                                `<ul class="border border-yellow-400">
-                                <li class="p-3 text-yellow-400">${billStatus}</li>
-                                </ul>`
-                            }
-
-                    else {
-                        return statusContainer = 
-                                `<ul class="border border-slate-400">
-                                <li class="p-3 text-slate-400">${billStatus}</li>
-                                </ul>`
-                            }
-
-            } */
-
     return (
         <div>
         {invoiceStatus === 'Paid' && <ul className="border border-green-400 rounded-lg">
