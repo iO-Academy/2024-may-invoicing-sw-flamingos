f80a728fa6c418432af6f4ede367e0cf119c4a57
Andy Bool <andybool50@gmail.com>
2024-07-02 11:38:42 +0100

    started to create the statusBox component, added props from story1task3 fetch

---
+++ src/components/Statusbox/index.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react"
+
+export default Statusbox(invoiceStatus, invoiceDue) {
+
+
+                const dueDate = {invoiceDue}
+                const invoiceStatus = {invoiceStatus}
+
+                let statusContainer = ''
+
+            function status(() => {
+                if ({invoiceStatus} === 'Paid') {
+statusContainer = <div className="border border-green-400">
+    <h3 className="p-3 text-green-400">{invoiceStatus</h3>
+</div>
+                }
+
+            )}
+
+    return (
+
+    )
+}
