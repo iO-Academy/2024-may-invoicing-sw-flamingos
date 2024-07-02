499b6f3f0c942d01ec5ba98535d3ec9e9f9babd4
Andy Bool <andybool50@gmail.com>
2024-07-02 12:09:02 +0100

    created ul tag to insert bullet point in status container

--- src/components/Statusbox/index.jsx
+++ src/components/Statusbox/index.jsx
@@ -11,7 +11,9 @@ export default Statusbox(invoiceStatus, invoiceDue) {
             function status(() => {
                 if (invoiceStatus === 'Paid') {
             statusContainer = '<div className="border border-green-400">
-                                <h3 className="p-3 text-green-400">invoiceStatus</h3>
+                                <ul>
+                                <li className="p-3 text-green-400">invoiceStatus</li>
+                                </ul>
                                 </div>'
                 }
 
