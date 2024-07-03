bcbcd1692757ecd545ce732a4941e65610fd5111
Seonaid Davis <seonaid_10@yahoo.co.uk>
2024-07-03 17:01:15 +0100

    made invoice details component and displayed detail array

--- src/components/InvoiceDetails/index.jsx
+++ src/components/InvoiceDetails/index.jsx
@@ -1,9 +1,23 @@
 import { useEffect, useState } from "react"
 
-export default function InvoiceDetails({desc, quant, cost, subTotal, paidToDate}) {
+export default function InvoiceDetails({ desc, quant, cost, subTotal, paidToDate }) {
 
 
     return (
-<p></p>
+        <div>
+            <div>
+                <p>Description</p>
+                <p>Quantity</p>
+                <p>Rate</p>
+                <p>Total</p>
+            </div>
+            <div>
+                {desc}
+                {quant}
+                {cost}
+                {subTotal}
+            </div>
+            <p>Paid to date {paidToDate}</p>
+        </div>
     )
 }
