53cb498e4d23e9fa62e5a3f1187d46007a770339
Florent Cuer <florentcuer@gmail.com>
2024-07-04 19:56:41 +0100

    Added working filter button, needs styling

--- src/components/InvoiceList/index.jsx
+++ src/components/InvoiceList/index.jsx
@@ -1,40 +1,42 @@
 import { useEffect, useState } from "react"
 import InvoiceSummary from "../InvoiceSummary"
-import SortByStatus from "../SortByStatus"
 
 export default function InvoiceList() {
 
     const [invoices, setInvoices] = useState([])
+    const [filterInvoices, setFilterInvoices] = useState(NaN)
+
     useEffect(() => {
-        fetch('https://invoicing-api.dev.io-academy.uk/invoices')
+        fetch(`https://invoicing-api.dev.io-academy.uk/invoices?status=${filterInvoices}`)
             .then(res => res.json())
             .then(invoiceData => {
                 setInvoices(invoiceData.data)
             }
             )
-    }, [])
+    }, [filterInvoices])
+
+    function filterStatus(e) {
+        setFilterInvoices(Number(e.target.value))
+    }
 
     return (
-        <div className="flex flex-col items-center px-16 bg-slate-50">
-            <SortByStatus />
-            {invoices.map(invoice => {
-                if (invoice.status_name === 'Paid') {
-                return (
-                    <InvoiceSummary key={invoice.id} invoiceID={invoice.invoice_id} invoiceTotal={invoice.invoice_total} invoiceName={invoice.name} invoiceStat={invoice.status_name} invoiceDue={invoice.due} />
-                )}
-                //if (invoice.status_name === 'Pending') {
-                //    return (
-                //        <InvoiceSummary key={invoice.id} invoiceID={invoice.invoice_id} invoiceTotal={invoice.invoice_total} invoiceName={invoice.name} invoiceStat={invoice.status_name} invoiceDue={invoice.due} />
-                //    )}
-                //    if (invoice.status_name === 'Overdue') {
-                //        return (
-                //            <InvoiceSummary key={invoice.id} invoiceID={invoice.invoice_id} invoiceTotal={invoice.invoice_total} invoiceName={invoice.name} invoiceStat={invoice.status_name} invoiceDue={invoice.due} />
-                //        )}
-                //      if (invoice.status_name === 'Cancelled') {
-                //           return (
-                //                <InvoiceSummary key={invoice.id} invoiceID={invoice.invoice_id} invoiceTotal={invoice.invoice_total} invoiceName={invoice.name} invoiceStat={invoice.status_name} invoiceDue={invoice.due} />
-                //            )}
-            })}
+        <div className="flex flex-col items-center bg-slate-50">
+            <div>
+                <select onChange={filterStatus}>
+                    <option value="NaN">Filter by Status</option>
+                    <option value="1">Filter by Paid</option>
+                    <option value="2">Filter by Pending</option>
+                    <option value="3">Filter by Cancelled</option>
+                    <option value="NaN">No Filter</option>
+                </select>
+            </div>
+            <div className="pr-20">
+                {invoices.map(invoice => {
+                    return (
+                        <InvoiceSummary key={invoice.id} invoiceID={invoice.invoice_id} invoiceTotal={invoice.invoice_total} invoiceName={invoice.name} invoiceStat={invoice.status_name} invoiceDue={invoice.due} />
+                    )
+                })}
+            </div>
         </div>
     )
 }
--- src/components/InvoiceSummary/index.jsx
+++ src/components/InvoiceSummary/index.jsx
@@ -2,7 +2,7 @@ import StatusBox from "../StatusBox";
 
 export default function InvoiceSummary({ invoiceID, invoiceTotal, invoiceName, invoiceStat, invoiceDue }) {
     return (
-        <div className="text-sm flex flex-col relative mx-10 my-2 p-4 bg-white box-border w-full border-2 border-grey min-[760px]:max-w-[850px] min-[760px]:flex-row">
+        <div className="text-sm flex flex-col relative mx-10 my-2 p-4 bg-white box-border w-full border-2 border-grey min-[760px]:max-w-[850px] min-[760px]:flex-row min-[760px]:pr-20">
             <div className="flex flex-col justify-between min-[760px]:justify-around min-[760px]:flex-row ">
                 <div className="flex flex-row items-center justify-between min-[760px]:justify-around min-[760px]:w-[400px]">
                     <p className="font-bold">#{invoiceID}</p>
